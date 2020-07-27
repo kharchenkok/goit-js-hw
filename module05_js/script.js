efa4bd2182de7d31498ab774ddf474ee631cc3bf
kharchenkok <k.v.kharchenko@ukr.net>
2020-07-28 00:35:35 +0300

    module 05 task 03 added

--- module05_js/script.js
+++ module05_js/script.js
@@ -66,4 +66,48 @@
 
 // poly.getInfo(); // User Poly is 3 years old and has 17 followers
 
-================================task-03========================================
+// ================================task-03========================================
+// // Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. 
+// // При виклику буде отримувати один аргумент - початковий масив товарів, 
+// // і записувати його у властивість items.
+
+// // Додай методи класу:
+
+// // getItems() - повертає масив поточних товарів
+// // addItem(item) - отримує новий товар і додає його до поточних
+// // removeItem(item) - отримує товар і, якщо він є, видаляє його з поточних
+// class Storage{
+//     constructor(itemsArr){
+//         this.items = itemsArr
+//     }
+//     getItems(){
+//         return this.items
+//     }
+//     addItem(item){
+//         return this.items.push(item)
+//     }
+//     removeItem(item){
+//         if (this.items.includes(item)){
+//             this.items.splice(this.items.indexOf(item),1)
+//         }
+//         return this.items
+//     }
+// }
+
+
+
+// const storage = new Storage([
+//   'Нанітоіди',
+//   'Пролонгер',
+//   'Залізні жупи',
+//   'Антигравітатор',
+// ]);
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+// storage.addItem('Дроїд');
+// console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+// storage.removeItem('Пролонгер');
+// console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
